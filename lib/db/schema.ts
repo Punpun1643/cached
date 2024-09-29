0bdb8691751d2e7678561913255e1ac9d9ab0efe
Papattarada Apithanangsiri <papattarada1643@gmail.com>
2024-09-29 14:35:41 +0800

    feat(db): configure db and update schema

--- lib/db/schema.ts
+++ lib/db/schema.ts
@@ -15,9 +15,10 @@ export const urls = pgTable('urls', {
   status: statusEnum('status').notNull(),
   tag: varchar('tag', { length: 128 }),
   address: text('address').notNull(),
-  dateAdded: timestamp('date_added').notNull()
+  dateAdded: timestamp('date_added').notNull().defaultNow()
 })
 
 // Export db types for other components
 export type Url = typeof urls.$inferSelect
+export type InsertUrl = typeof urls.$inferInsert
 
