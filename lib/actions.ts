933b8168296852fb27127de5f14eee27eb6f15a7
Papattarada Apithanangsiri <papattarada1643@gmail.com>
2024-10-15 23:32:58 +0800

    feat: automatically get title from url

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,12 +1,17 @@
 'use server'
 
-import { revalidatePath } from "next/cache";
+import { revalidatePath } from "next/cache"
 import { addUrl } from "./db/queries"
-import { InsertUrl } from "./db/schema";
+import { InsertUrl } from "./db/schema"
+import * as cheerio from 'cheerio'
 
 export async function handleAddUrl(address: string, tag: string) {
+  const $ = await cheerio.fromURL(address)
+  const addressTitle = $('title').text() || ""
+  
+
   const urlData: Omit<InsertUrl, "id"> = {
-    title: "just testing", // Plain string // TODO: automatically obtain title using metadata
+    title: addressTitle, // Plain string // TODO: automatically obtain title using metadata
     status: "pending", // Plain string
     tag: tag, // Plain string
     address: address, // Plain string
