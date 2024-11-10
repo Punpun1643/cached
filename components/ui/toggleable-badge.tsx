dea85517e3d7ef1a0b23af4560662196b4bb4a41
Papattarada Apithanangsiri <papattarada1643@gmail.com>
2024-11-10 18:12:08 +0800

    refactor: remove commented code

--- components/ui/toggleable-badge.tsx
+++ components/ui/toggleable-badge.tsx
@@ -38,27 +38,4 @@ const ToggleableBadge = <T extends SelectUrl, U extends string>({ url, options, 
   )
 }
 
-// const ToggleableBadge = ({ url }: { url : SelectUrl }) => {
-//   const handleUpdateUrlWithIdAndStatus = async (newStatus: StatusEnum) => {
-//      await handleUpdateUrlStatus(url.id, newStatus)
-//   }
-//
-//   return (
-//     <Select onValueChange={handleUpdateUrlWithIdAndStatus}>
-//       <SelectTrigger>
-//         <Badge variant="outline" className="capitalize cursor-pointer">
-//           <SelectValue placeholder={url.status} />
-//         </Badge>
-//       </SelectTrigger>
-//       <SelectContent>
-//         {StatusEnum.options.map((option) => (
-//           <SelectItem key={option} value={option}>
-//             {option}
-//           </SelectItem>
-//         ))}
-//       </SelectContent>
-//     </Select>
-//   )
-// }
-
 export { ToggleableBadge }
